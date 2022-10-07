8d704438e6aab92e134cc7fd99a5215c87d8bb63
cklewar@t-online.de <cklewar@t-online.de>
2022-10-07 11:58:28 +0200

    update version

--- versions.tf
+++ versions.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.2.7"
+  required_version = ">= 1.3.0"
   cloud {
     organization = "cklewar"
     hostname     = "app.terraform.io"
@@ -10,11 +10,10 @@ terraform {
   }
 
   required_providers {
-    volterra = {
-      source  = "volterraedge/volterra"
-      version = ">= 0.11.12"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.25.0"
     }
-
     local = ">= 2.2.3"
     null  = ">= 3.1.1"
   }
